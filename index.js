568d09585279f1c4c95aef6b70e3c06eb463aeea
Mustafa Jhar <jharmostafa@gmail.com>
2025-06-09 17:59:05 +0300

    finished

---
+++ index.js
@@ -0,0 +1,30 @@
+const getColors = document.getElementById('get-colors');
+const colorsContainer = document.getElementById('colors-container');
+
+getColors.addEventListener('click', async() => {
+    
+    const hexCode = document.querySelector('input[type="color"]').value.slice(1);
+    const colorMode = document.querySelector('select').value.toLowerCase();
+    const url = `https://www.thecolorapi.com/scheme?hex=${hexCode}&format=json&mode=${colorMode}&count=5`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    colorsContainer.innerHTML = data.colors.map((color) => {
+        return (
+            `
+                <div class="color-slot">
+                    <div data-hex-code="${color.hex.value}" style="background-color: ${color.hex.value};" class="colors"></div>
+                    <p data-hex-code="${color.hex.value}" class="hex-codes">${color.hex.value}</p>
+                </div>
+            `
+        )
+    }).join('');
+
+        
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target.dataset.hexCode === null) return;
+    navigator.clipboard.writeText(e.target.dataset.hexCode);
+} )
+
